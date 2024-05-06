84a61dec4b1542f8152e5973e69b1110da18cc3f
Prateek Dash <prateekdash50@gmail.com>
2024-05-06 13:10:49 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Bhubaneswar");
